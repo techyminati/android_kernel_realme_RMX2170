28318b6b042f1352de36043cd615bf96280588f6
techyminati <sinha.aryan03@gmail.com>
2021-04-12 12:11:55 +0000

    sound/ : Import from realme7pro-Q-oss
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -2820,12 +2820,12 @@ static int snd_soc_dapm_add_route(struct snd_soc_dapm_context *dapm,
 		wsource = wtsource;
 
 	if (wsource == NULL) {
-		dev_err(dapm->dev, "ASoC: no source widget found for %s\n",
+		dev_dbg(dapm->dev, "ASoC: no source widget found for %s\n",
 			route->source);
 		return -ENODEV;
 	}
 	if (wsink == NULL) {
-		dev_err(dapm->dev, "ASoC: no sink widget found for %s\n",
+		dev_dbg(dapm->dev, "ASoC: no sink widget found for %s\n",
 			route->sink);
 		return -ENODEV;
 	}
@@ -2930,7 +2930,7 @@ int snd_soc_dapm_add_routes(struct snd_soc_dapm_context *dapm,
 	for (i = 0; i < num; i++) {
 		r = snd_soc_dapm_add_route(dapm, route);
 		if (r < 0) {
-			dev_err(dapm->dev, "ASoC: Failed to add route %s -> %s -> %s\n",
+			dev_dbg(dapm->dev, "ASoC: Failed to add route %s -> %s -> %s\n",
 				route->source,
 				route->control ? route->control : "direct",
 				route->sink);
